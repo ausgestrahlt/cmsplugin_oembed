aa77458ea86d7717133fe35f42bd4f85d4dd6f19
Dimitri <dev@yare.fr>
2020-08-09 12:58:13 +0200

    added on_delete in migration file

--- cmsplugin_oembed/migrations/0001_initial.py
+++ cmsplugin_oembed/migrations/0001_initial.py
@@ -7,21 +7,27 @@ import embed_video.fields
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('cms', '0012_auto_20150607_2207'),
-    ]
+    dependencies = [("cms", "0012_auto_20150607_2207")]
 
     operations = [
         migrations.CreateModel(
-            name='VideoPluginModel',
+            name="VideoPluginModel",
             fields=[
-                ('cmsplugin_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
-                ('name', models.CharField(max_length=50, blank=True)),
-                ('video_url', embed_video.fields.EmbedVideoField()),
+                (
+                    "cmsplugin_ptr",
+                    models.OneToOneField(
+                        on_delete=models.deletion.CASCADE,
+                        parent_link=True,
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        to="cms.CMSPlugin",
+                    ),
+                ),
+                ("name", models.CharField(max_length=50, blank=True)),
+                ("video_url", embed_video.fields.EmbedVideoField()),
             ],
-            options={
-                'abstract': False,
-            },
-            bases=('cms.cmsplugin',),
-        ),
+            options={"abstract": False},
+            bases=("cms.cmsplugin",),
+        )
     ]
